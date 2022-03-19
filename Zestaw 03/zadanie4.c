896ee713ec7d656e969df273f2eb0e554668a157
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2022-03-19 13:36:20 +0100

    Zestaw 03 - wstępny pomysł

---
+++ Zestaw 03/zadanie4.c
@@ -0,0 +1,73 @@
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define ROZMIAR_DATAGRAMU 65527
+
+int gniazdko = -1;
+
+void zamknijPolaczenie () {
+	if (gniazdko != -1)
+		close (gniazdko);
+}
+
+int main (int argc, char const *argv[]) {
+	
+	int port = atoi (argv[1]);
+	
+	atexit (zamknijPolaczenie);
+	
+	struct sockaddr_in adres;
+    adres.sin_family = AF_INET;
+    adres.sin_port = htons(port);
+    adres.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	gniazdko = socket(AF_INET, SOCK_DGRAM, 0);
+	if (gniazdko == -1) {
+		perror ("Blad socket()");
+		exit (EXIT_FAILURE);
+	}
+	
+	if (bind (gniazdko, (struct sockaddr*)&adres, sizeof(struct sockaddr_in)) == -1) {
+		perror ("Blad bind()");
+		exit (EXIT_FAILURE);
+	}
+	
+    while (1) {
+		
+		struct sockaddr_in klient;
+        socklen_t klientRozmiar = sizeof(klient);
+		
+		char bufor[ROZMIAR_DATAGRAMU];
+		unsigned long int suma = 0;
+		char odczytanaLiczba[ROZMIAR_DATAGRAMU];
+		
+        if (recvfrom(gniazdko, bufor, sizeof(bufor), 0, (struct sockaddr *)&klient, &klientRozmiar) == -1) {
+			perror ("Blad recvfrom()");
+            exit (EXIT_FAILURE);
+		}
+		
+		if (bufor[0] == '\n') {
+			
+		} else {
+			for (int i = 1, j = 0; i < ROZMIAR_DATAGRAMU || bufor[i] == '\n' || bufor[i - 1] == '\r' && bufor[i] == '\n') {
+				
+				if (bufor[i] == ' ' && bufor[i - 1] != ' ') {
+					odczytanaLiczba[j] = '\0';
+					suma += atoi (odczytanaLiczba);
+					j = 0;
+					odczytanaLiczba = itoa (suma);
+					if (sendto(gniazdko, odczytanaLiczba, ROZMIAR_DATAGRAMU, 0, (struct sockaddr *)&klient, klientRozmiar) == -1) {
+						perror ("Blad sendto()");
+						exit (EXIT_FAILURE);
+					}
+				} else {
+					odczytanaLiczba[j] = bufor[i];
+				}
+			}
+		}
+    }
+}
